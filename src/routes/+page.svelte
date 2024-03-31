fa927885b35b2f9dde0435991b6bdb8c88d49b55
Zion <ziongonet@gmail.com>
2024-03-31 16:12:50 +0100

    changes made to listing component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Listing from '../lib/Listing.svelte';
   import Hero from '../lib/Hero.svelte'
   export let data;
 
@@ -8,15 +7,15 @@
 <Hero />
 <div>
   
-  <ul>
+  <ul >
       {#each data?.records as record}
       <a href="{record.url}">
-        <li class="flex  justify-between max-w-screen-md gap-x-6 py-5 bg-white flex-wrap items-center  mx-auto">
-          <div class="flex min-w-0 gap-x-4 items-center ">
+        <li class="flex px-3 justify-between max-w-screen-md gap-x-6 py-5 border border-gray-200 mb-3 rounded-lg flex-wrap items-center mx-auto">
+          <div class="flex min-w-0 gap-x-4 items-center">
             <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src='http://127.0.0.1:8090/api/files/jobs/{record.id}/{record.picture}' alt="">
             <div class="min-w-0 flex-auto">
-              <span class="text-purple-800 text-sm">{record.companyname}</span>
-              <h3 class="text-sm leading-6 text-gray-900 font-bold mt-px">{record.title}</h3>
+              <span class="text-purple-800 text-sm ">{record.companyname}</span>
+              <h3 class="md:text-sm leading-6 text-gray-900 font-bold mt-px max-w-xs">{record.title}</h3>
               <p class="mt-1 truncate text-xs leading-5 text-gray-500">{record.city}, {record.country}</p>
               <div class="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-secondary/80 text-purple-700 bg-purple-100">
                 {record.tag}
@@ -24,7 +23,7 @@
             </div>
           </div>
           <div class="flex flex-col items-end text-sm leading-6 text-gray-900 sm:flex-col-reverse ">
-            <span class="mt-1 text-gray-500 text-sm flex">Posted on August 24, 2023</span>
+            <span class="mt-1 text-gray-500 text-sm flex">Aug 24</span>
             <!-- <span class="mt-1 text-gray-500 text-sm flex">Promoted</span> -->
           </div>
         </li>
