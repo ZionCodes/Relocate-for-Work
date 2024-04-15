fab86a7bcad99cdada6d7c307b714bb05d111b64
Zion <ziongonet@gmail.com>
2024-04-15 14:28:10 +0100

    remove border from apply button

--- src/routes/jobs/[slug]/+page.svelte
+++ src/routes/jobs/[slug]/+page.svelte
@@ -55,7 +55,7 @@
       </div>
     </div>
   </div>
-  <div class="mt-5 flex lg:ml-4 lg:mt-0 border border-green-400">
+  <div class="mt-5 flex lg:ml-4 lg:mt-0">
     <span class="sm:ml-3">
       <button type="button" class="inline-flex items-center rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm bg-primary-700 border-primary-600 hover:bg-primary-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
         Apply now
