3658882a7de4ad7d36645b93c3a1ada9dc929a96
Zion <ziongonet@gmail.com>
2024-04-16 15:11:01 +0100

    added google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,13 @@
 	import "../app.pcss";
 	import Footer from "../lib/Footer.svelte";
 	import Navbar from "../lib/Navbar.svelte";
+	import Analytics from '$lib/analytics.svelte'
 </script>
 
+
+<Analytics />
 <Navbar />
 <slot></slot>
 <Footer />
 
+  
