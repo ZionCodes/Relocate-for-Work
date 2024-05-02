b9c7a7425a666aa42618c8e94de0ddf1c557e67e
Zion <ziongonet@gmail.com>
2024-05-03 00:33:30 +0100

    apply button

--- src/routes/jobs/[slug]/+page.svelte
+++ src/routes/jobs/[slug]/+page.svelte
@@ -71,16 +71,6 @@
 <div class="parent-container p-2 max-w-4xl items-center mx-auto">
   <p class="mb-3 text-gray-500 dark:text-gray-400">{@html data.records.description}</p>
 </div>
-<div class="mt-5 flex lg:ml-4 lg:mt-0">
-  <span class="sm:ml-3">
-    <a href={data.records.url}>
-      <button type="button" class="inline-flex items-center rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm bg-primary-700 border-primary-600 hover:bg-primary-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-        Apply now
-      </button>
-    </a>
-    
-  </span>
-</div>
 
 
 <Email />
