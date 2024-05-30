800182b3b26959d9b1ec91b09509cf1937ea1128
Zion <ziongonet@gmail.com>
2024-05-30 05:42:28 +0100

    changed email file name

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 import { SECRET_EMAIL, SECRET_PASSWORD } from '$env/static/private';
-import { createEmail } from '$lib/server/emailHandler';
+import { createEmail } from '$lib/server/EmailHandler';
 
 
 export async function load({ params }) {
